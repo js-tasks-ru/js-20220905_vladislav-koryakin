c7e94522af7302407b619737ebab1fd26fa803c7
weepzip <weepzip6669@gmail.com>
2022-09-15 19:10:04 +0300

    3rd module

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,11 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  let props = path.split('.');
 
+  return function getValue(object) {
+    return (props.length > 1 && object.hasOwnProperty(props[0])) 
+      ? getValue(object[props.shift()]) 
+      : object[props.shift()];
+  };
 }
